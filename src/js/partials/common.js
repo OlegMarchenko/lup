90ef00e7438b48b99acf12a07d84241f3b5197ae
OlegMarchenko <workolegmarchenko@gmail.com>
2018-02-13 09:43:04 +0200

    add candyskins presetation page

--- src/js/partials/common.js
+++ src/js/partials/common.js
@@ -3,4 +3,27 @@ $(document).ready(function () {
     // Mask for callback input
     $('.phone').mask('+00(000)000-00-00');
 
+    //Portfolio selector
+    var types = ['gamble', 'mobile-web', 'mobile-app', 'e-commerce', 'web-app'];
+    $('#portfolio-select').on('change', (function(e){
+        var value = e.target.value;
+        console.log(value);
+        types.forEach(function(type){
+            var cards;
+            if(type!==value){
+                cards = document.getElementsByClassName(type);
+                [].forEach.call(cards, function (card){
+                    card.style.display = 'none';
+                })
+            } else {
+                cards = document.getElementsByClassName(type);
+                Array.prototype.forEach.call(cards, function (card){
+                    card.style.display = 'block';
+                })
+            }
+        })
+
+    }));
+
+
 });
