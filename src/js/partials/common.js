9f5dea9c0d2485152a032d3f387a4ebcf6de4733
OlegMarchenko <workolegmarchenko@gmail.com>
2018-02-16 09:26:22 +0200

    add filter selector on portfolio page

--- src/js/partials/common.js
+++ src/js/partials/common.js
@@ -3,34 +3,44 @@ $(document).ready(function () {
     // Mask for callback input
     $('.phone').mask('+00(000)000-00-00');
 
+    //===================================================================
 
 
     //Portfolio selector
-    var types = ['gamble', 'mobile-web', 'mobile-app', 'e-commerce', 'web-app'];
-    $('#portfolio-select').on('change', (function(e){
+    var types = ['gamble', 'mobile-app', 'e-commerce', 'web-app'];
+    $('#portfolio-select').on('change', (function (e) {
         var value = e.target.value;
-        console.log(value);
-        types.forEach(function(type){
+        types.forEach(function (type) {
             var cards;
-            if(type!==value){
-                cards = document.getElementsByClassName(type);
-                [].forEach.call(cards, function (card){
-                    card.style.display = 'none';
+            if (type === value || value === 'all') {
+                cards = type === 'all' ? $('.card-project')  : $(`.${type}`);
+                cards.each(function (index, card) {
+                    card.style.display = 'block';
                 })
             } else {
-                cards = document.getElementsByClassName(type);
-                Array.prototype.forEach.call(cards, function (card){
-                    card.style.display = 'block';
+                //cards = document.getElementsByClassName(type);
+                cards = $('.' + type);
+                cards.each(function (index, card) {
+                    card.style.display = 'none';
                 })
             }
         })
 
     }));
 
+    var navLinks = ['active', 'commerce', 'health', 'gambling', 'estate', 'logistics', 'finance'];
+    $('.portfolio-nav a').on('click', function (cls) {
+        var clsName = cls.target.className;
+        navLinks.forEach(function (link) {
 
-    /*Stop roulette animation*/
-    $('.roulette').hover(function () {
-        $(this).stop(true).fadeTo(200, 1)
+
+        });
+        console.log(clsName);
+
     })
+
+
+    //===================================================================
+
 
 });
